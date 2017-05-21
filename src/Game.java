ab01bb712a294a433defb1ebaea326ec29371f35
buithuynhung <buithuynhung1997@gmail.com>
2017-05-21 03:27:27 +0300

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,74 @@
+import java.awt.*;
+
+public class Game {
+    private int xPipe, yPipe = 0, widthPipe = 90, space2 = 100,
+            xBird, yBird, sizeBird = 30;
+    private Color colorPipe = new Color(50, 235, 50),
+            colorBird = Color.YELLOW;
+    private Pipes pipe;
+    private Bird bird;
+    private Size size = new Size();
+    private int score;
+    private Boolean goOverpipe1 = false, goOverpipe2 = false, goOverpipe3 = false;
+
+
+    public Game() {
+        xPipe = size.widthScreen - pipe.getWidth();
+        xBird = size.widthMFrame / 5;
+        yBird = size.heightMFrame / 3;
+        pipe = new Pipes(xPipe, yPipe, widthPipe, colorPipe, space2);
+        bird = new Bird(xBird, yBird, sizeBird, colorBird);
+    }
+
+    public void score() {
+        if (bird.getX() + bird.getSize() > pipe.getX1()
+                && bird.getX() < pipe.getX1() + pipe.getWidth())
+            goOverpipe1 = false;
+        if (!goOverpipe1 && bird.getX() - pipe.getX1() > pipe.getWidth()) {
+            score++;
+            goOverpipe1 = true;
+        }
+
+        if (bird.getX() + bird.getSize() > pipe.getX2()
+                && bird.getX() < pipe.getX2() + pipe.getWidth())
+            goOverpipe2 = false;
+        if (!goOverpipe2 && bird.getX() - pipe.getX2() > pipe.getWidth()) {
+            score++;
+            goOverpipe2 = true;
+        }
+
+        if (bird.getX() + bird.getSize() > pipe.getX3()
+                && bird.getX() < pipe.getX3() + pipe.getWidth())
+            goOverpipe3 = false;
+        if (!goOverpipe3 && bird.getX() - pipe.getX3() > pipe.getWidth()) {
+            score++;
+            goOverpipe3 = true;
+        }
+    }
+
+    public boolean gameOver() {
+        boolean touchFloor = bird.touchFloor();
+        boolean touchPipes = bird.touchPipes();
+        if (touchFloor || touchPipes) return true;
+        return false;
+    }
+
+    public void step() {
+        if (!gameOver()) {
+            pipe.scroll(5);
+            bird.moveDown();
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Pipes getPipe() {
+        return pipe;
+    }
+
+    public Bird getBird() {
+        return bird;
+    }
+}
